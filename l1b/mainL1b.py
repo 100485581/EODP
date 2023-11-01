c4d36f31f0004cef4d04f642803a639481f29bea
100485581 <100485581@alumnos.uc3m.es>
2023-11-01 01:21:05 +0100

    EODP Laura

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -5,8 +5,8 @@ from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\LAU\EODP\auxiliary'
-indir = r"C:\LAU\EODP_TER\EODP-TS-L1B\input"
-outdir = r"C:\LAU\EODP_TER\EODP-TS-L1B\OutputsLaura"
+indir = r"C:\LAU\EODP_TER\EODP-TS-E2E\OutputsLaura"
+outdir = r"C:\LAU\EODP_TER\EODP-TS-E2E\OutputsLaura_L1b"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
